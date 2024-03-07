67cb9bd178b044a3dead704da9f022dcaaa43225
forest <alexeidorman@gmail.com>
2024-03-07 11:18:52 -0800

    add constant in property

--- src/main/java/com/example/forest/Configuration.java
+++ src/main/java/com/example/forest/Configuration.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties("limit-service")
+@ConfigurationProperties("forest")
 public class Configuration {
 
 	private int maximum;
--- src/main/java/com/example/forest/controller/ForestConfigurationController.java
+++ src/main/java/com/example/forest/controller/ForestConfigurationController.java
@@ -1,16 +1,21 @@
 package com.example.forest.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.forest.Configuration;
 import com.example.forest.bean.ForestConfiguration;
 
 @RestController
 public class ForestConfigurationController {
 	
+	@Autowired
+	private Configuration configuration;
+	
 	@GetMapping("/limits")
 	public ForestConfiguration retrieveForestConfigurations() {
-		return new ForestConfiguration(100, 1);
+		return new ForestConfiguration(configuration.getMaximum(), configuration.getMinimum());
 	}
 
 }
